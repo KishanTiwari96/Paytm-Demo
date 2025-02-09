b170316937d54854fbe495c43033bcc9eff15530
KishanTiwari96 <kishantiwari698591@gmail.com>
2025-02-09 13:38:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ const mainRouter = require("./routes/index")
 const app = express();
 const jsonwebtoken = require("jsonwebtoken")
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://paytm-demo-frontend.vercel.app', 
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+}));
 app.use(express.json());
 app.use("/api/v1",mainRouter);
 
