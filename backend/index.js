a541556c8e001c21dc2b6d60f5b10aa3c3f17bf9
KishanTiwari96 <kishantiwari698591@gmail.com>
2025-04-03 17:41:14 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,8 @@ const express = require("express");
 const cors = require("cors")
 const mainRouter = require("./routes/index")
 const app = express();
-const jsonwebtoken = require("jsonwebtoken")
 
-app.use(cors({
-    origin: 'https://paytm-demo-frontend.vercel.app', 
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-}));
+app.use(cors())
 app.use(express.json());
 app.use("/api/v1",mainRouter);
 
